06be3946db930b7a88c6ea521286607e012a46e1
raingang <raingang4@gmail.com>
2017-11-20 21:25:00 +0200

    Add Message class

---
+++ channels_chat/chat/messages.py
@@ -0,0 +1,32 @@
+import json
+
+
+class Message(dict):
+
+    def __init__(self, text=None, username=None, **kwargs):
+        defaults = {'type': 'message'}
+        if text:
+            defaults['text'] = text
+        if username:
+            defaults['username'] = username
+        defaults.update(kwargs)
+        self['text'] = json.dumps(defaults)
+
+
+class SystemMessage(Message):
+
+    def __init__(self, *args, **kwargs):
+        kwargs['type'] = 'system'
+        kwargs['username'] = 'System'
+        super().__init__(*args, **kwargs)
+
+
+def message(*args, **kwargs):
+    return Message(*args, **kwargs)
+
+
+def system(*args, **kwargs):
+    return SystemMessage(*args, **kwargs)
+
+
+
