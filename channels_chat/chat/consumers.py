4080126f78155a0adc6a53b445bc6ffe39acfc27
raingang <raingang4@gmail.com>
2017-11-20 21:25:34 +0200

    Change consumers

--- channels_chat/chat/consumers.py
+++ channels_chat/chat/consumers.py
@@ -1,21 +1,19 @@
 import json
-from channels import Group 
+from channels import Group
+from . import messages
+from channels.sessions import channel_session
 
 def ws_connect(message):
 	message.reply_channel.send({'accept': True})
 	Group('global').add(message.reply_channel)
-	content = {'text': json.dumps({"action": "post_message",
-		                           "username": 'Server',
-		                           "data": 'Connection established.'})}
-	message.reply_channel.send(content)
+	data = messages.system(text = 'Connection established.')
+	message.reply_channel.send(data)
 
 def ws_message(message):
 	response = json.loads(message['text'])
-	data = response['data']
-	content = {'text': json.dumps({'action': 'post_message',
-		                           'username': 'Unknown',
-		                           'data': data,})}
-	Group('global').send(content)
+	text = response['text']
+	data = messages.message(username = 'Unknown', text = text)
+	Group('global').send(data)
 
 def ws_disconnect(message):
 	Group('global').discard(message.reply_channel)
