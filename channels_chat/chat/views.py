82a8effb589c3971022e9d29874deed054f024e7
raingang <raingang4@gmail.com>
2017-11-22 02:19:46 +0200

    Update index view

--- channels_chat/chat/views.py
+++ channels_chat/chat/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.views import generic
+from .forms import NameForm
+from django.http import HttpResponseRedirect
 
-class IndexView(generic.TemplateView):
-	template_name = 'index.html'
 
+def index(request):
+	if 'username' in request.session:
+		return render(request, 'index.html', )
+	else: 
+		if request.method == 'POST':
+			form = NameForm(request.POST)
+			if form.is_valid():
+				username = form.cleaned_data['username']
+				request.session['username'] = username
+				return HttpResponseRedirect('/')
+
+		else:
+			form = NameForm()
+		return render(request, 'register.html', {'form' : form})
